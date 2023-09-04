174216fd29533fbf1ebb00f237dd52240525f34c
Casper Putz <casper.putz@gmail.com>
2023-09-04 17:58:03 +0200

    Adapt scripts to using the new fixed coefficients system

--- data/script/to_magma.sh
+++ data/script/to_magma.sh
@@ -1,2 +1,4 @@
+# this script converts output files of the hunter_search program to file which can be loaded into MAGMA
+
 #!/bin/sh
 grep -o '\[R.*\]' | grep -o '\[1.*\]' | sed '$!s/$/,/' | sed '1s/^/pols := [\n/' | sed '$a];'
